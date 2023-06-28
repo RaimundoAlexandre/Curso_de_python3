4ccef04ce8cb4b81c8194665dde16c71d4054f19
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-06-28 09:05:34 -0300

    Alinhamento
    arrumei o alinhamento pra ficar visualmente agradavel

--- mundo 3/Listagem de preços.py
+++ mundo 3/Listagem de preços.py
@@ -8,10 +8,11 @@ print("=-=" * 20)
 listagem = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90,
             'Estojo', 25.00, 'Transferidor', 4.00, 'Compasso', 9.99,
             'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
-for c in range(0,len(listagem),2):
-    itens = listagem[c]
-    preco = listagem[c+1]
-    print(f"{itens}{'.......'*5}{'R$':>8} {preco:.2f}")
+
+for c in range(0, len(listagem), 2):
+    item = listagem[c]
+    preco = listagem[c + 1]
+    print(f"{item:.<30} R$ {preco:>6.2f}") # Alinhamento usando :.<30 e >6.2f, dependendo do resultado pode ser necessario ajustes
 
 print("=-=" * 20)
 print("\n")
