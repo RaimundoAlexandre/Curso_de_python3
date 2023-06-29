507f44b98756dc7e03d495837293de0763f81053
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-06-29 14:33:53 -0300

    upadet
    atualização do codigo, nada de importante só uma atualização pra melhorar a legibilidade

--- mundo 3/lista com maior e menor.py
+++ mundo 3/lista com maior e menor.py
@@ -2,9 +2,25 @@ valores = []
 
 for c in range(0,5):
   valores.append(int(input("Digite os valores: ")))
+print("=-="*10)
 
 for c, v in enumerate(valores):
   print(f"na posição {c} encontrei {v}")
-print(f"O maior valor encontrado foi {max(valores)}")
-print(f"O menor valor encontrado foi {min(valores)}")
+
+print("=-="*10)
+maior = max(valores)
+print(f"O maior valor encontrado foi {maior} na posição",end=' ')
+for c , p in enumerate(valores):
+  if p == maior:
+    print(c,end='... ')
+print("")
+
+menor = min(valores)
+print(f"O menor valor encontrado foi {menor} na posição",end=' ')
+for c , p in enumerate(valores):
+  if p == menor:
+    print(c,end='... ')
+print("")
+print("=-="*10)
+
 print("FIM")
