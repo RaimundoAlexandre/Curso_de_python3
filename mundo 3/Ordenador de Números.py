14d88ef29b8d5da5175df94127c16d3d74708177
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-06-29 23:36:23 -0300

    curso em video de python 3

--- mundo 3/Ordenador de Números.py
+++ mundo 3/Ordenador de Números.py
@@ -1,4 +1,6 @@
-numeros = []
+#numeros = []
+
+"""
 for c in range(0, 5):
     num = int(input("Digite um valor: "))
     if c == 0 or num >= numeros[-1]:
@@ -11,3 +13,26 @@ for c in range(0, 5):
 
 print("=-=" * 10)
 print(numeros)
+
+"""
+
+numeros = []  # Lista vazia para armazenar os valores
+
+for c in range(0, 5):
+    n = int(input("Digite um valor: "))
+
+    # Condição para verificar se é o primeiro valor ou se o valor digitado é maior do que o último valor da lista
+    if c == 0 or n > numeros[-1]:
+        numeros.append(n)  # Adiciona o valor no final da lista
+    else:
+        pos = 0  # Variável para controlar a posição na lista
+        while pos < len(numeros):
+            # Verifica se o valor digitado é menor ou igual ao valor da posição atual na lista
+            if n <= numeros[pos]:
+                numeros.insert(pos, n)  # Insere o valor na posição adequada
+                break
+            pos += 1  # Incrementa a posição
+
+print("=-="*10)
+print(f"Os valores digitados em ordem foram: {numeros}")
+
--- mundo 3/Separador de Números Pares e Ímpares.py
+++ mundo 3/Separador de Números Pares e Ímpares.py
@@ -11,17 +11,14 @@ print(f"Os números digitados foram {numeros}")
 print("=-="*10)
 
 par = []
-
+impar = []
 for c, p in enumerate(numeros):
   if p % 2 == 0:
     par.append(numeros[c])
+  else:
+    impar.append(numeros[c])
 print(f"Os números pares são {par}")
 print("=-="*10)
-
-impar = []
-
-for c, im in enumerate(numeros):
-  if im % 2 != 0:
-    impar.append(numeros[c])
+   
 print(f"Os numeros impares são {impar}")
 print("=-="*10)
