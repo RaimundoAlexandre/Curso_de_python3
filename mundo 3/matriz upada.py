e84248b9b91b8d2a086e87badc12cf8a42fd1e4e
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-07-03 12:00:04 -0300

    att

--- mundo 3/matriz upada.py
+++ mundo 3/matriz upada.py
@@ -1,3 +1,4 @@
+"""
 matriz = []
 soma_p = 0
 soma_c = 0
@@ -31,3 +32,29 @@ print("=-="*10)
 print(f"A soma dos números pares é {soma_p}")
 print(f"A somo dos valores da terceira coluna e {soma_c}")
 print(f"O maior valor da segunda coluna é {maior}")
+"""
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+spar = maior = scol = 0
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f"Digite um número para a  posiçãp {l} {c}: "))
+
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f"[{matriz[l][c]:^5}]",end='')
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
+    print()
+print(f"A soma dos valores pares é {spar}")
+
+for l in range(0, 3):
+    scol += matriz[l][2]
+print(f"A soma dos valres da terceira coluna é {scol}")
+
+for c in range(0, 3):
+    if c == 0:
+        mair = matriz[1][c]
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
+print(f"O maior valor da segunda linha é {maior}")
