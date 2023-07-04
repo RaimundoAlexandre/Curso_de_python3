1fe93c55c8220cb33c84fd5c6afd7921d47caedb
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-07-04 19:42:58 -0300

    att

--- mundo 2/Sequencia de fibonacci.py
+++ mundo 2/Sequencia de fibonacci.py
@@ -4,3 +4,16 @@ while a < n:
   print(a, end=' ↦ ')
   a, b = b, a+b
 print(n)
+"""
+n = int(input("Valor: "))
+f = 0
+n1 = 0
+n2 = 1
+cont = 1
+while cont <=n:
+  print(f,end=' ')
+  n1 = n2
+  n2 = f
+  f = n1 + n2
+  cont += 1
+"""
