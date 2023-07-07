942e1d9d5b58a069b0fd59f7f36a0c969c7e3d2c
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-07-06 22:48:16 -0300

    joguinho
    jogo da forca, pretendo melhorar esse jogo e implementar uma interface, porem ainda estou estudando a biblioteca tkinter, tenho um curso de kivy mais esse vai demorar um pouco mais

---
+++ mundo 3/Jogo da forca.py
@@ -0,0 +1,52 @@
+from random import randint
+from time import sleep
+
+print("###" * 20)
+print(f"{'JOGO DA FORCA':^60}")
+print("###" * 20)
+print("\n")
+print("=-=" * 20)
+
+nomes = ['Cachorro', 'Programar', 'Estudar', 'Python', 'Curso', 'Gratis', 'Estudar', 'Praticar', 'Trabalhar', 'Video game']
+
+# Loop principal do jogo
+while len(nomes) > 0:
+    print(f"Vou sortear uma dessas palavras {nomes}\nTente adivinhar qual palavra foi sorteada")
+    indice = randint(0, len(nomes) - 1)
+    p = nomes[indice]
+    nomes.pop(indice)
+
+    quantP = len(p)
+    cont = 15
+    print(f"Você tem {cont} chances")
+
+    print(f"Dica: é uma palavra de {len(p)} letras")
+    print("---" * 20)
+
+    while True:
+        if cont == 0:
+            print("Você não tem mais tentativas. Você Perdeu.")
+            break
+        jogador = str(input("Tente adivinhar: ")).strip().lower()
+        cont -= 1
+        print("---" * 20)
+        if jogador in p.lower():
+            quantP -= 1
+            print("Você acertou!")
+            print("Qual a próxima letra?")
+            print("---" * 20)
+            if quantP == 0:
+                print("Você venceu!! Parabéns!")
+                break
+        else:
+            print(f"Você errou. Restam {cont} chances. Tente novamente.")
+            print("---" * 20)
+
+    print(f"A palavra sorteada foi: {p.capitalize()}")
+
+    # Pergunta ao jogador se deseja jogar novamente
+    jogar_novamente = input("Deseja jogar novamente? (S/N): ").strip().upper()
+    if jogar_novamente != 'S':
+        break
+
+print("Obrigado por jogar! Até a próxima.")
