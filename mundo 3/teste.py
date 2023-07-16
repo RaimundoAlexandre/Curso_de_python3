169fe6486483b7e589b015c6a41d00734ffe2111
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-07-16 02:33:24 -0300

    att

--- mundo 3/teste.py
+++ mundo 3/teste.py
@@ -1,18 +1,1 @@
-from tkinter import *
-
-janela = Tk()
-janela.geometry("300x400+200+200")
-janela.title("Calculadora")
-
-
-
-saida = Label(janela, text='testeteste')
-saida.place(x=100,y=150)
-
-btSoma = Button(janela,width=10, text='+')
-btSoma.place(x=100, y=100)
-
-
-
-
-janela.mainloop()
+import 
